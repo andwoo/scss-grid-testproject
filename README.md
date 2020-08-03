41546fac3fef8f97080e6a62a84b3344f7e6de31
Andrew M <andrewlmanley@gmail.com>
2020-08-03 09:58:02 -0400

    Added scss-grid submodule

---
+++ README.md
@@ -0,0 +1,9 @@
+# Setup
+- Install git submodules by running the following command from the project root.
+```
+git submodule update --init --recursive
+```
+- Install depedencies by running the following terminal command from the project root.
+```
+yarn
+```
--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import * as React from 'react';
 import { hot } from 'react-hot-loader';
+import { Column } from '@andwoo/scss-grid';
 
 class App extends React.Component {
   render(): JSX.Element {
-    return <p>Hello There!</p>;
+    return (
+      <Column>
+        <p>Hello There!</p>
+      </Column>
+    );
   }
 }
 
